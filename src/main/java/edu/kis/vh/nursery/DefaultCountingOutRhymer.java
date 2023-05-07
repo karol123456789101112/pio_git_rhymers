900fcc9297ab4a21af76a699bb03e17e3ac98b40
Matenar <240736@edu.p.lodz.pl>
2023-05-07 12:38:20 +0200

    6. Zmiana liczb na Deklaracje stałe

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int CAPACITY = 12;
+    public static final int INDEX = -1;
+    public static final int DEFAULT_RETURN_VALUE= -1;
+    private int[] numbers = new int[CAPACITY];
 
-    public int total = -1;
+    public int total = INDEX;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == INDEX;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == CAPACITY-1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return DEFAULT_RETURN_VALUE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return DEFAULT_RETURN_VALUE;
         return numbers[total--];
     }
 
